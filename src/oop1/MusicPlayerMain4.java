3e5f1cb7a104912f4f03aa5361e7697acb781d9d
rim109 <fla7722@naver.com>
2024-08-02 07:55:52 +0900

    feat: 객체지향프로그래밍 2

---
+++ src/oop1/MusicPlayerMain4.java
@@ -0,0 +1,15 @@
+package oop1;
+
+public class MusicPlayerMain4 {
+    public static void main(String[] args) {
+        MusicPlayer data = new MusicPlayer();
+
+        data.on();
+        data.off();
+        data.plusVolume();
+        data.plusVolume();
+        data.minusVolume();
+        data.statusVolume();
+        data.off();
+    }
+}
