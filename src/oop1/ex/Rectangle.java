ac367be7fff7f5067cb9889963999f2f60965010
rim109 <fla7722@naver.com>
2024-08-02 08:05:36 +0900

    feat: 답 --> 객체 지향 프로그래밍

--- src/oop1/ex/Rectangle.java
+++ src/oop1/ex/Rectangle.java
@@ -1,4 +1,19 @@
 package oop1.ex;
 
 public class Rectangle {
+    int width;
+    int height;
+
+    int calculateArea(){
+        return width * height;
+    }
+
+    int calculatePerimeter(){
+        return 2 * (width + height);
+    }
+
+    boolean isSquare(){
+        return  width == height;
+    }
+    // 여기서는 매개변수를 굳이 쓰지 않아도 된다.
 }
