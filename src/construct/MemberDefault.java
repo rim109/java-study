ef2001c3daebb50c5d07aed6e01871da158c4a09
rim109 <fla7722@naver.com>
2024-08-02 09:27:10 +0900

    feat: 기본 생성자

---
+++ src/construct/MemberDefault.java
@@ -0,0 +1,13 @@
+package construct;
+
+public class MemberDefault {
+    String name;
+
+    MemberDefault(){
+
+    }
+
+    // 생성자가 하나라도 있으면 자바는 기본 생성자를 만들지 않는다.
+    // 기본 생성자는 자동적으로 만들어준다.. 왜?
+    // 생성자 기능이 필요하지 않는 경우에도 모든 클래스에 개발자가 직접 기본 생성자를 정의해야하므로,,,
+}
