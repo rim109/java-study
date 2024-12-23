268c6e4cda239046793a803e8c5f8f679073d2a2
rim109 <132591560+rim109@users.noreply.github.com>
2024-12-24 08:34:40 +0900

    fix: 오타수정

--- src/access/BankAccount.java
+++ src/access/BankAccount.java
@@ -7,7 +7,7 @@ public class BankAccount {
         balance = 0;
     }
 
-    // public 메시드: deposit
+    // public 메서드: deposit
     public void deposit(int amount){
         if (isAmountValid(amount)){
             balance += amount;
@@ -16,7 +16,7 @@ public class BankAccount {
         }
     }
 
-    // public 메시드: withdraw
+    // public 메서드: withdraw
     public void withdraw(int amount){
         if (isAmountValid(amount) && balance - amount >= 0){
             balance -= amount;
