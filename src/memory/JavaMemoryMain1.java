9cdd752fec19e0096b555137f3205fb80be2f234
rim109 <fla7722@naver.com>
2025-01-02 23:30:42 +0900

    feat: 스택 영역

---
+++ src/memory/JavaMemoryMain1.java
@@ -0,0 +1,24 @@
+package memory;
+
+public class JavaMemoryMain1 {
+    public static void main(String[] args) {
+        System.out.println("main start");
+        method1(10);
+        System.out.println("main end");
+    }
+
+    static void method1(int m1){
+        System.out.println("method1 start");
+        int cal = m1 *2 ; // 지역변수
+        method2(cal);
+        System.out.println("method1 end");
+    }
+
+    static void method2(int m2){
+        System.out.println("method2 start");
+        System.out.println("method2 end");
+    }
+
+    // 스택 구조
+    // main -> method1 -> method2 -> method2 -> method1 -> main
+}
